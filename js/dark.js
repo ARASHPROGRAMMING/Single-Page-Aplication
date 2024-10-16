75e35d125f4c56fa33a02d5583a9655694c7218b
ARASHGHANABRZADEH <arashghanbarzadeh0902@gmail.com>
2024-10-16 12:51:20 +0330

    Responsive header finish

--- js/dark.js
+++ js/dark.js
@@ -3,10 +3,10 @@ const modeIcon = document.querySelector(".material-symbols-outlined");
 const body = document.querySelector("body");
 
 darkMode.addEventListener("click", () => {
-    if (body.classList.toggle("dark__body")) {
-      modeIcon.textContent = "dark_mode";
-        body.style.transition = "all 300ms linear";
-  }else {
+  if (body.classList.toggle("dark__body")) {
+    modeIcon.textContent = "dark_mode";
+    body.style.transition = "all 300ms linear";
+  } else {
     modeIcon.textContent = "light_mode";
   }
 });
