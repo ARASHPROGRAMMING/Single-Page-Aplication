c3bc49442be5d5d5f555dc45532c2329d0e2ba40
ARASHGHANABRZADEH <arashghanbarzadeh0902@gmail.com>
2024-10-18 22:56:24 +0330

    add card cours to body site

---
+++ js/data.js
@@ -0,0 +1,41 @@
+const dataBaseSite = [
+  {
+    id: 1,
+    image: "../images/htmlCourse.jpg",
+    title: "آموزش html صفر تا صد مقدماتی تا پیشرفته",
+    caption: "آموزش html اولین راه برای  <br> طراحی و ساخت سایت",
+    master: "آرش قنبرزاده",
+    masterLogo: "../images/personLogo.svg",
+    time: "14:00:00",
+    timeLogo: "../images/Time.svg",
+    price: "رایگان",
+    priceLog: "../images/moneyLogo.svg",
+  },
+
+  {
+    id: 1,
+    image: "../images/htmlCourse.jpg",
+    title: "آموزش html صفر تا صد مقدماتی تا پیشرفته",
+    caption: "آموزش html اولین راه برای  <br> طراحی و ساخت سایت",
+    master: "آرش قنبرزاده",
+    masterLogo: "../images/personLogo.svg",
+    time: "14:00:00",
+    timeLogo: "../images/Time.svg",
+    price: "رایگان",
+    priceLog: "../images/moneyLogo.svg",
+  },
+
+  {
+    id: 1,
+    image: "../images/htmlCourse.jpg",
+    title: "آموزش html صفر تا صد مقدماتی تا پیشرفته",
+    caption: "آموزش html اولین راه برای  <br> طراحی و ساخت سایت",
+    master: "آرش قنبرزاده",
+    masterLogo: "../images/personLogo.svg",
+    time: "14:00:00",
+    timeLogo: "../images/Time.svg",
+    price: "رایگان",
+    priceLog: "../images/moneyLogo.svg",
+  },
+
+];
---
+++ js/main.js
@@ -0,0 +1,24 @@
+const root = document.querySelector(".root");
+
+function renderCard() {
+  let card = dataBaseSite
+    .map(item => {
+      return `
+            <div class="card">
+                <img id="img__card" src="${item.image}" alt="" width="325" height="200">
+              <h3 id="card__title">${item.title}</h3>
+              <p id="card__caption">${item.caption}</p>
+              <h4 id="card__master"><img src="${item.masterLogo}" alt="" width="30" height="30">${item.master}</h4>
+              <h5 id="card__time">${item.time}<img src="${item.timeLogo}" alt=""></h5>
+              <hr id="card__hr">
+              <h4 id="card__price">${item.price}<img src="${item.priceLog}" alt="" width="30" height="30"></h4>
+              </div>
+        
+        `;
+    })
+    .join("");
+
+  root.innerHTML = card;
+}
+
+renderCard()
