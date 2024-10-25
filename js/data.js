c44bc330fdbc65d1ebbf27969c2229bc7d8f7223
ARASHGHANABRZADEH <arashghanbarzadeh0902@gmail.com>
2024-10-25 15:45:11 +0330

    add card cours

--- js/data.js
+++ js/data.js
@@ -13,7 +13,7 @@ const dataBaseSite = [
   },
 
   {
-    id: 1,
+    id: 2,
     image: "../images/htmlCourse.jpg",
     title: "آموزش html صفر تا صد مقدماتی تا پیشرفته",
     caption: "آموزش html اولین راه برای  <br> طراحی و ساخت سایت",
@@ -26,7 +26,46 @@ const dataBaseSite = [
   },
 
   {
-    id: 1,
+    id: 3,
+    image: "../images/htmlCourse.jpg",
+    title: "آموزش html صفر تا صد مقدماتی تا پیشرفته",
+    caption: "آموزش html اولین راه برای  <br> طراحی و ساخت سایت",
+    master: "آرش قنبرزاده",
+    masterLogo: "../images/personLogo.svg",
+    time: "14:00:00",
+    timeLogo: "../images/Time.svg",
+    price: "رایگان",
+    priceLog: "../images/moneyLogo.svg",
+  },
+
+  {
+    id: 4,
+    image: "../images/htmlCourse.jpg",
+    title: "آموزش html صفر تا صد مقدماتی تا پیشرفته",
+    caption: "آموزش html اولین راه برای  <br> طراحی و ساخت سایت",
+    master: "آرش قنبرزاده",
+    masterLogo: "../images/personLogo.svg",
+    time: "14:00:00",
+    timeLogo: "../images/Time.svg",
+    price: "رایگان",
+    priceLog: "../images/moneyLogo.svg",
+  },
+
+  {
+    id: 5,
+    image: "../images/htmlCourse.jpg",
+    title: "آموزش html صفر تا صد مقدماتی تا پیشرفته",
+    caption: "آموزش html اولین راه برای  <br> طراحی و ساخت سایت",
+    master: "آرش قنبرزاده",
+    masterLogo: "../images/personLogo.svg",
+    time: "14:00:00",
+    timeLogo: "../images/Time.svg",
+    price: "رایگان",
+    priceLog: "../images/moneyLogo.svg",
+  },
+
+  {
+    id: 6,
     image: "../images/htmlCourse.jpg",
     title: "آموزش html صفر تا صد مقدماتی تا پیشرفته",
     caption: "آموزش html اولین راه برای  <br> طراحی و ساخت سایت",
